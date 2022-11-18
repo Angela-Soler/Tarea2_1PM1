67910e77247ec0d2dea998301cbf5d842607941a
Angela Soler <an_soler@hotmail.com>
2022-11-17 22:31:53 -0600

    Initial commit

---
+++ app/src/main/java/com/finsol/tarea2_1pm1/MainActivity.java
@@ -0,0 +1,87 @@
+package com.finsol.tarea2_1pm1;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int TOMA_VIDEO = 1;
+    private VideoView vv1;
+    private ArrayList<String> lista=new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        vv1=findViewById(R.id.videoView);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1000);
+        }
+
+
+    }
+
+    public void tomarVideo(View v)
+    {
+        Intent intent=new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        startActivityForResult(intent,TOMA_VIDEO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==TOMA_VIDEO && resultCode==RESULT_OK)
+        {
+            Uri videoUri=data.getData();
+            vv1.setVideoURI(videoUri);
+            vv1.start();
+            try {
+                AssetFileDescriptor videoAsset = getContentResolver().openAssetFileDescriptor(data.getData(), "r");
+                FileInputStream in = videoAsset.createInputStream();
+                String nombrearch=crearNombreArchivoMP4();
+                FileOutputStream archivo = openFileOutput(nombrearch, Context.MODE_PRIVATE);
+                byte[] buf = new byte[1024];
+                int len;
+
+                while ((len = in.read(buf)) > 0) {
+                    archivo.write(buf, 0, len);
+                }
+            }catch (IOException e)
+            {
+                Toast.makeText(this,"Problemas en la grabación",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private String crearNombreArchivoMP4() {
+
+        String fecha = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String nombre = fecha + ".mp4";
+        return nombre;
+    }
+
+
+}
